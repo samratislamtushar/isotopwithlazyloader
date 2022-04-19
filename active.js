915aee7bdbda877ed8d70f075f20e6752922c8d5
Md Samrat Islam Tushar <44712138+mdsamratislamtushar@users.noreply.github.com>
2022-04-19 21:54:49 +0600

    Create active.js

---
+++ active.js
@@ -0,0 +1,26 @@
+// isotop
+	$('.grid').imagesLoaded(function () {
+		// init Isotope
+		var $grid = $('.grid').isotope({
+			itemSelector: '.grid-item',
+			percentPosition: true,
+			masonry: {
+				// use outer width of grid-sizer for columnWidth
+				columnWidth: '.grid-item',
+			}
+		});
+
+
+		// filter items on button click
+		$('.portfolio-menu').on('click', 'button', function () {
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({ filter: filterValue });
+		});
+
+		//for menu active class
+		$('.portfolio-menu button').on('click', function (event) {
+			$(this).siblings('.active').removeClass('active');
+			$(this).addClass('active');
+			event.preventDefault();
+		});
+	});
